4f2202bcf71985104b3da90d32270b965664263b
musthafaUk <ukmusthafau@gmail.com>
2020-10-12 20:19:08 +0530

    chnge

--- app/screens/TaskScreen.js
+++ app/screens/TaskScreen.js
@@ -13,53 +13,58 @@ import {
 import Carousel, { Pagination } from "react-native-snap-carousel";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 const SCREEN_WIDTH = Dimensions.get("window").width;
-import { withNavigation } from 'react-navigation';
-import * as RootNavigation from '../navigation/NavigationService';
-import  data from '../mock/tasks';
- class TaskScreen extends React.Component {
+import { withNavigation } from "react-navigation";
+import * as RootNavigation from "../navigation/NavigationService";
+import data from "../mock/tasks";
+class TaskScreen extends React.Component {
   static navigationOptions = {
-    title: 'Task',
+    title: "Task",
   };
-  
+
   constructor(props) {
     super(props);
     this.state = {
-      
       activeIndex: 0,
-     carouselItems:data
+      carouselItems: data,
     };
   }
 
   _renderItem({ item }) {
     return (
-      <TouchableWithoutFeedback style={styles.container}
-      onPress={() => RootNavigation.navigate('Content', { items: item })}
+      <TouchableWithoutFeedback
+        style={styles.container}
+        onPress={() => RootNavigation.navigate("Content", { items: item })}
       >
-        <View style={{ flexDirection: "row" }}>
+        <View style={styles.img}>
+          <Image
+            source={{
+              uri: item.taskUrl,
+            }}
+            style={styles.logo}
+          />
+        </View>
+        <View style={styles.text}>
           <Text style={styles.heading}>{item.taskName}</Text>
         </View>
-        <Image
-          source={{
-            uri:item.taskUrl
-          }}
-          style={styles.logo}
-        />
       </TouchableWithoutFeedback>
     );
   }
 
   render() {
-
     return (
       <SafeAreaView style={styles.bg}>
-        {/* <View style={{ flexDirection: "row" , justifyContent:"center"}}>
-          <AntDesign name="dingding" size={34} color="#6951ae" />
-          <Text style={{color:"white", fontWeight:"bold", fontSize:20}}>BoreButton</Text>
-
-        </View> */}
-        <View
-          style={{ flex: 1, flexDirection: "row", justifyContent: "center" }}
-        >
+        <View style={styles.headRow}>
+          <Text style={styles.bore}>BoreBUtton</Text>
+          <View style={{paddingLeft:50}}>
+              <View style={styles.imgRound}>
+                <Image
+                style={styles.imgProfile}
+                source={require('../assets/5.jpg')}
+                />
+              </View>
+          </View>
+        </View>
+        <View style={styles.carouselStyle}>
           <Carousel
             layout={"default"}
             ref={(ref) => (this.carousel = ref)}
@@ -87,47 +92,75 @@ import  data from '../mock/tasks';
 }
 
 const styles = StyleSheet.create({
-  back: {
-    position: "absolute",
-    bottom: 10,
-    right: 58,
-  },
-
   bg: {
     backgroundColor: colors.primary,
-    paddingTop: 75,
+    paddingTop: 45,
     flex: 1,
   },
-
+  bore:{
+    color:colors.white,
+    fontSize:40,
+    fontWeight:"bold",
+    shadowColor:colors.light,
+  },
   container: {
-    margin: 40,
-    marginBottom: 30,
-    backgroundColor: colors.white,
-    height: 500,
-    borderRadius: 30,
-    elevation: 8,
+    backgroundColor: colors.primary,
+    height: 550,
     alignItems: "center",
-    padding: 15,
-    overflow:"hidden"
+    paddingBottom: 50,
+    // overflow: "hidden",
+    // elevation: 10,
+    margin: 30,
+    marginTop: 0,
+    borderRadius: 30,
+  },
+  carouselStyle: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
   },
   heading: {
     fontSize: 18,
-    padding: 5,
-    color: colors.secondary,
+    padding: 10,
+    color: colors.white,
     fontWeight: "bold",
   },
-
-  next: {
-    position: "absolute",
-    bottom: 5,
-    right: 8,
+  headRow:{
+    flexDirection:"row",
+    margin:10
   },
+  img: {
+    height: "100%",
+    width: "100%",
+    borderRadius: 30,
+    overflow: "hidden",
+    backgroundColor: colors.white,
+    elevation: 10,
+    margin: 10,
+  },
+  imgProfile:{
+    width:70,
+    height:70
+  },
+  imgRound:{
+    backgroundColor:"white",
+    width:70,
+    height:50,
+    borderRadius:30,
+    overflow:"hidden",
+    elevation:10
+  },
+  logo: {
+    width: "100%",
+    height: "100%",
+  },
+
   tabBar: {
-    position: "absolute",
-    right: 0,
-    bottom: 70,
-    left: 0,
+    position: "relative",
     backgroundColor: colors.primary,
+  },
+  text: {
+    flexDirection: "row",
   },
   ww: {
     width: 10,
@@ -135,11 +168,6 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginHorizontal: 8,
     backgroundColor: colors.white,
-  },
-  logo: {
-    width: "100%",
-    height:"80%",
-    margin:30,
   },
 });
 
